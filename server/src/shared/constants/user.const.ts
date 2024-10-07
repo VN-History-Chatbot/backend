84303ddb63024b5f6260c027f51c9d3fde0703a4
Ngoc Thach <thach.le@topebox.com>
2024-10-07 14:12:37 +0700

    fix bug role

--- server/src/shared/constants/user.const.ts
+++ server/src/shared/constants/user.const.ts
@@ -1,5 +1,5 @@
-export const ROLE_USER = "USER";
-export const ROLE_ADMIN = "ADMIN";
-export const ROLE_SUPER_ADMIN = "SUPERADMIN";
+export const ROLE_USER = "1";
+export const ROLE_ADMIN = "100";
+export const ROLE_SUPER_ADMIN = "999";
 
 export const DEFAULT_USER_ID = "default";
